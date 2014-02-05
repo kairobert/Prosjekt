3cca1f8ece545b56d229801b9d1e81fb04d68a0c
Magne Normann <student@Sanntid.(none)>
2014-02-05 19:15:20 +0100

    Added all io-funcs except listen-funcs

--- src/elevdriver/elevdriver.go
+++ src/elevdriver/elevdriver.go
@@ -10,6 +10,11 @@ import (
 
 type Direction int 
 
+const N_FLOORS 4
+
+const SPEED0 = 2048
+const SPEED1 = 4024
+
 const (
     UP Direction //= iota
     DOWN
@@ -22,63 +27,126 @@ type Button struct{
     floor int
 }
 
-buttonChan = make(chan Button)
-sensorChan = make(chan int)
-motorChan = make(chan Direction)
+var buttonChan chan Button
+var floorChan chan int
+var motorChan chan Direction
+var stopButtonChan chan bool
+var obsChan chan bool
 
-emgStopChan = make(chan bool)
-obstructionChan = make(chan bool)
 
-func listen_buttons(){
-}
-
-func listen_sensors({
-}
-
-func set_light(floor int, dir Direction){
+func setLight(floor int, dir Direction){
     switch{  
     case floor == 1 && dir == NONE:
             Set_bit(LIGHT_COMMAND1)
     case floor == 1 && dir == UP:
             Set_bit(LIGHT_UP1)
-
  	case floor == 2 && dir == NONE:
         Set_bit(LIGHT_COMMAND2)
     case floor == 2 && dir == UP:
         Set_bit(LIGHT_UP2)
     case floor == 2 && dir == DOWN:
         Set_bit(LIGHT_DOWN2)
-        
     case floor == 3 && dir == NONE:
         Set_bit(LIGHT_COMMAND3)
     case floor == 3 && dir == UP:
         Set_bit(LIGHT_UP3)
     case floor == 3 && dir == DOWN:
-        Set_bit(LIGHT_DOWN4)
-        
+        Set_bit(LIGHT_DOWN4)        
     case floor == 4 && dir == NONE:
         Set_bit(LIGHT_COMMAND4)
     case floor == 4 && dir == DOWN:
         Set_bit(LIGHT_DOWN4)
     default:
-        printf("Error: Illegal floor or dir")
+        printf("Error: Illegal floor or direction")
 }
 
-func clear_light(){
+func clearLight(floor int, dir Direction){
+    switch{  
+    case floor == 1 && dir == NONE:
+        Clear_bit(LIGHT_COMMAND1)
+    case floor == 1 && dir == UP:
+        Clear_bit(LIGHT_UP1)
+ 	case floor == 2 && dir == NONE:
+        Clear_bit(LIGHT_COMMAND2)
+    case floor == 2 && dir == UP:
+        Clear_bit(LIGHT_UP2)
+    case floor == 2 && dir == DOWN:
+        Clear_bit(LIGHT_DOWN2)      
+    case floor == 3 && dir == NONE:
+        Clear_bit(LIGHT_COMMAND3)
+    case floor == 3 && dir == UP:
+        Clear_bit(LIGHT_UP3)
+    case floor == 3 && dir == DOWN:
+        Clear_bit(LIGHT_DOWN4)   
+    case floor == 4 && dir == NONE:
+        Clear_bit(LIGHT_COMMAND4)
+    case floor == 4 && dir == DOWN:
+        Clear_bit(LIGHT_DOWN4)
+    default:
+        printf("elevdriver: Error! Illegal floor or direction!")
 }
 
-func clear_lights(){
-    for
-        clear_light(i)
+func clearAllLights(){
+    for int floor := 1; floor<N_FLOORS ; floor++{
+        for int dir := 1; dir<N_DIR; dir+{
+            clear_light(floor, dir)
+        }
+    }
 }
 
-func motor_ctrl(motorChan channel){ // fix channel type
+func motorCtrl(motorChan channel Direction){
+    lastDir := NONE
+    for {
+        switch newDir := <-motorChan{
+        case UP:
+            Clear_bit(MOTORDIR)
+            Write_analog(MOTOR,SPEED1)
+        case DOWN:
+            Set_bit(MOTORDIR)
+            Write_analog(MOTOR,SPEED1)
+        case NONE:
+            if lastDir == DOWN{
+                Clear_bit(MOTORDIR)
+                Write_analog(MOTOR,SPEED0)
+            } 
+            else if lastDir == UP{
+                Set_bit(MOTORDIR)
+                Write_analog(MOTOR,SPEED0)
+            } else{
+            fmt.Println("elevdriver: ERROR, illegal lastDir")
+        default:
+            Write_analog(MOTOR,SPEED0)
+            fmt.Println("elevdriver: ERROR, illegal motor direction")
+        }
+        lastDir = newDir
+    }
+}
+
+func listenButtons(){
+}
+
+func listenSensors({
 }
 
 func InitElev(){
+
+    if !IoInit(){
+        fmt.Println("elevdriver: Driver init()... OK!")
+	} else {
+	    fmt.Println("elevdriver: Driver init()... FAILED!")
+	}
 	
-	go listen_buttons(buttonChan)
-	go listen_sensors(sensorChan)
+	clearAllLights();
+	
+	buttonChan = make(chan Button)
+    sensorChan = make(chan int)
+    motorChan = make(chan Direction)
+
+    emgStopChan = make(chan bool)
+    obstructionChan = make(chan bool)
+	
+	go listenButtons(buttonChan)
+	go listenSensors(sensorChan)
 	
 	go func() {
     // capture ctrl+c and stop elevator
@@ -86,8 +154,44 @@ func InitElev(){
         signal.Notify(c, os.Interrupt)
         s := <-c
         log.Printf("Got: %v, terminating program..", s)
-        //Set motorspead = 0
+        MotorStop()
         clearLights()
         os.Exit(1)
     }()
 }
+
+func OpenDoor(){
+    Set_bit(DOOR_OPEN)
+}
+
+func CloseDoor(){
+    Set_bit(DOOR_CLOSE)
+}
+
+func MotorUp() {
+        motorChan <- UP
+}
+
+func MotorDown() {
+        motorChan <- DOWN
+}
+
+func MotorStop() {
+        motorChan <- NONE
+}
+
+func GetStopButton(){
+    return Read_bit(STOP)
+}
+
+func SetStopButton(){
+    Set_bit(STOP)
+}
+
+func ClearStopButton(){
+    Clear_bit(STOP)
+}
+
+
+
+
