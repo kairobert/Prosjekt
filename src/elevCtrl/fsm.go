d092595701ae59d1c31ae16e624260955ff7f218
larsgus <larsgus@stud.ntnu.no>
2014-02-20 10:50:47 +0100

    added fsm_handle_events

--- src/elevCtrl/fsm.go
+++ src/elevCtrl/fsm.go
@@ -29,12 +29,14 @@ const(
 func (elev *Elevator)action_start_down(){
 	elevdriver.MotorDown(elev.motorChan)
 	elev.state = MOVING_DOWN
+	elev.lastDir = elevdriver.DOWN
 	fmt.Println("fsm: MOVING_DOWN")
 }
 
 func (elev *Elevator)action_start_up(){
 	elevdriver.MotorUp(elev.motorChan)
 	elev.state = MOVING_UP
+	elev.lastDir = elevdriver.UP
 	fmt.Println("fsm: MOVING_UP")
 }
 
@@ -68,13 +70,13 @@ func action_dummy(){
 }
 
 func (elev *Elevator)fsm_init(){
-	//state						//start_down			//start_up				//exec_order			//timeout			//stop			//obst	
-	IDLE_ev			:= []func(){elev.action_start_down, elev.action_start_up, 	elev.action_exec_order,	action_dummy,		action_dummy,	action_dummy}	
-	DOORS_OPEN_ev	:= []func(){action_dummy, 			action_dummy, 			action_dummy,			elev.action_done,	action_dummy,	action_dummy}	
-	MOVING_UP_ev	:= []func(){action_dummy, 			elev.action_exec_order, elev.action_start_up,	action_dummy,		action_dummy,	action_dummy}
-	MOVING_DOWN_ev	:= []func(){action_dummy, 			elev.action_exec_order,	elev.action_start_up,	action_dummy,		action_dummy,	action_dummy} 
-	EMG_ev			:= []func(){action_dummy, 			action_dummy, 			action_dummy,			action_dummy,		action_dummy,	action_dummy}
-	OBST_ev			:= []func(){action_dummy, 			action_dummy, 			action_dummy,			action_dummy,		action_dummy,	action_dummy}
+	//state						//start_down			//start_up				//exec_order			//timeout			
+	IDLE_ev			:= []func(){elev.action_start_down, elev.action_start_up, 	elev.action_exec_order,	action_dummy}	
+	DOORS_OPEN_ev	:= []func(){action_dummy, 			action_dummy, 			action_dummy,			elev.action_done}	
+	MOVING_UP_ev	:= []func(){action_dummy, 			action_dummy, 			elev.action_stop,		action_dummy}
+	MOVING_DOWN_ev	:= []func(){action_dummy, 			action_dummy,			elev.action_stop,		action_dummy} 
+	EMG_ev			:= []func(){action_dummy, 			action_dummy, 			action_dummy,			action_dummy}
+	OBST_ev			:= []func(){action_dummy, 			action_dummy, 			action_dummy,			action_dummy}
 
 	table := [][]func(){
 		IDLE_ev, 
@@ -91,44 +93,48 @@ func (elev *Elevator)fsm_update(event Event_t){
 	elev.fsm_table[elev.state][event]()
 }
 
-/*
-type Elevator struct{
-	fsm_table	[][]func()
-	state 		State_t
-	lastDir 	elevdriver.Direction_t
-	lastFloor 	int
-    buttonChan 	chan elevdriver.Button
-    floorChan 	chan int
-    motorChan 	chan elevdriver.Direction_t
-    stopButtonChan chan bool
-    obsChan 	chan bool
-*/
+func (elev *Elevator)should_stop(floor int) bool{
+	//communicate with orders and check if current floor got pending orders in correct direction
+	stop := true	
+	return stop
+}
 
-/*
-func (elev *Elevator)fsm_get_event(){
+func (elev *Elevator)get_nearest_order() elevdriver.Direction_t{
+	//communicate with orders and get next direction
+	return elevdriver.UP
+}
+
+
+func (elev *Elevator)fsm_handle_events(){
 	select{
-	case <- elev.stopButton:
+
+	case <- elev.stopButtonChan:
+		elev.fsm_update(stop)
+
 	case <- elev.obsChan:
+		elev.fsm_update(obst)
+
 	case floor:=<- elev.floorChan:
 		if floor != -1{
 			elev.lastFloor = floor
-			if should_stop(){
-				event chan <- exec_order
+			if elev.should_stop(floor){
+				elev.fsm_update(exec_order)
 			}
-		}	
+		}
+	
 	case <- elev.newOrder:
-		target_floor:= get_nearest_order()
-		switch{
-		case target_floor<elev.lastFloor && elev.state		
+		nextDir := elev.get_nearest_order()
+		if elev.state == IDLE{
+			switch nextDir{
+			case elevdriver.UP:
+				elev.fsm_update(start_down)	
+	
+			case elevdriver.DOWN:
+				elev.fsm_update(start_up)
+
+			case elevdriver.NONE:
+				elev.fsm_update(exec_order)
+			}
+		}
 	}
 }
-
-*/
-
-
-
-
-
-
-
-
